e251c2f2adec3d8b391823f398dac89b44035a72
GAWD <kiengm88@gmail.com>
2019-04-18 06:05:43 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,11 +33,14 @@ var checkForMatch = function () {
  }
 }
 
-var flipCard = function (cardId) {
+
+var flipCard = function () {
+ var cardId = this.getAttribute('data-id');
+ this.setAttribute('src', cards[cardId].cardImage);
  if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[2]) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
-	}
+	} 
 	else {
 		alert("Sorry, try again.");
 	}
@@ -48,6 +51,17 @@ var flipCard = function (cardId) {
  console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+	 var cardElement = document.createElement('img');
+	 cardElement.setAttribute('src', 'images/back.png');
+	 cardElement.setAttribute('data-id', i);
+	 cardElement.addEventListener('click', flipCard);
+	 document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+
+createBoard();
 checkForMatch();
+
